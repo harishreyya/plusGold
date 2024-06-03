bd448b490b64d0980f2d685c5c33718382c355d6
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-06-04 00:28:20 +0530

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -36,6 +36,109 @@ export const Footer = ()=>{
 
         </div>
 
+        <div className="horizontal-line"></div>
+<br/>
+<div className="flex play-store">
+  <p>© FinShakti Solutions Pvt. Ltd.</p>
+  <img src={require("../Images/app_store.png")} alt=""/>
+
+</div>
+
+<div className="faqs">
+
+<div>
+    <h3>What is a Gold Savings Scheme?</h3>
+    <p>A gold savings scheme or gold investment scheme is a financial instrument that enables individuals to invest in gold systematically over a period of time. At Plus Gold, India’s first jewellery savings app, we offer various gold schemes to cater to the diverse needs of gold investors and buyers with varying budgets.</p>
+        
+    <p>  These schemes work on the principle of accumulating gold gradually, making them accessible to all seeking to invest in gold. PlusGold allows investors to choose from different schemes like the golden bonanza scheme, wedding saving scheme, anniversary gift scheme, gold monthly scheme, and many other gold savings schemes, each tailored to suit their preferences and financial goals.</p>
+         
+    <p>  These schemes often offer flexibility in terms of payment frequencies and duration, allowing investors to choose the most suitable plan according to their financial capabilities. By choosing in a gold savings scheme, individuals can diversify their investment portfolio, hedge against inflation, and accumulate gold in a disciplined manner, thereby fostering long-term financial security,</p>
+
+
+
+    <h3>Features of Gold Investment Scheme</h3>
+    <p>Gold investment schemes offered by PlusGold offer various features, including flexible investment options, assured returns, and the convenience of online transactions. Some other features of opting for PlusGold’s gold investment plans include:</p>
+
+    <ul>
+     <li><b>0 Lock-in period:</b> Investing in gold schemes with PlusGold comes with no lock-in period, meaning you can withdraw it anytime you want.vv</li>
+     <li><b>Safe & Secure:</b> With PlusGold’s gold investment schemes, your gold is 100% secured in regulated vaults.  </li>
+     <li><b>Instant Withdrawal:</b> Our gold investment schemes allow for instant cash withdrawal into your account.</li>
+     <li><b>Flexibility of one-time or SIP:</b> If you opt for our golden bonanza scheme, it gives you the flexibility to buy gold through one-time or SIP. Plus, you get 10% extra gold, meaning you get 1gm extra gold on the purchase of every 10gm.  </li>
+
+    </ul>
+
+    <p>Our gold investment plans are designed to cater to the diverse needs of investors, ensuring maximum benefits.</p>
+</div>
+
+
+<div>
+    <h3>Why Invest in Gold?</h3>
+    <p>Investing in gold brings several benefits to the table. Here is why you should invest in gold:</p>
+
+    <p><b>Gold holds its value over time:</b><br/>
+Gold is a smart investment because it helps protect your money from losing value when prices go up. While the value of currency goes down as prices of commodities rise, gold’s worth tends to stay steady or even increase, protecting your investment in the long run. That is why investors consider gold as one of the most trusted assets for investment. By investing your hard earned money in gold, you can preserve wealth, ensuring a brighter and more financially secure future.</p>
+
+<p><b>Universal appeal:</b><br/>
+Gold holds universal appeal due to its recognized value across the globe, which distinguishes it from other assets. Its widespread acceptance as a form of investment ensures seamless conversion to cash in different parts of the world. This attribute makes gold a preferred choice for investors seeking liquidity and stability in their portfolios. Additionally, its enduring status as a symbol of wealth and security further solidifies its universal value.</p>
+
+<p><b>Higher liquidity than real estate:</b><br/>
+
+While real estate properties appreciate in value over time, selling them at desirable prices may take time, from months to years. But that is not the case with gold even in its physical form. Gold investments provide faster liquidity compared to other physical assets. Additionally, investment in gold comes with no lock-in period except for sovereign gold bonds (SGBs). That means investors can resell gold with ease anytime they need cash.
+</p>
+
+<p><b>Easy to Invest:</b> <br/>
+Buying gold is like investing your money in something solid like coins or jewellery. While assets stored digitally are susceptible to hacking and other misuse, gold remains safe from such concerns. Plus, investing in gold is easy, and it gives you a sense of security. These attributes make gold a safer and more secure investment.</p>
+
+<p><b>Gold gives stable returns:</b><br/>
+Gold is a great addition to your investments because it tends to move independently from stocks and bonds. This means that when other investments are doing poorly, gold can help balance out your portfolio and reduce overall risk. By including gold in your investment mix, you can potentially improve your chances of seeing stable returns over time.</p>
+
+
+
+</div>
+
+<div>
+    <h3>How to Invest in Gold Scheme?</h3>
+    <p>A gold scheme is more or less similar to a monthly investment plan, but it allows investors to buy gold on their investments instead of getting interest. It offers a safe avenue for portfolio diversification and hedge against inflation. Individuals seeking to invest in gold may use the PlusGold app. PlusGold is a hassle-free platform that enables Indian households to invest in gold with convenience and transparency.</p>
+
+    <p>To begin investing, simply sign up on our app, choose from our range of gold investment schemes tailored to suit your future gold purchase and financial goals, deposit your funds, and watch your investment grow.</p>
+
+    <p>Whether you prefer traditional gold purchase plans or digital gold options, we offer flexible solutions to cater to your preferences. With competitive pricing, secure transactions, instant withdrawal, and many more benefits, PlusGold ensures a seamless investment experience.</p>
+    <p>Start building your wealth today with our trusted gold investment schemes and unlock the potential of this timeless asset. Leverage PlusGold for your gold investment needs and embark on a journey towards financial security and prosperity.</p>
+
+
+</div>
+
+<div>
+    <h3>How Plus Gold Works?</h3>
+    <p>PlusGold is a revolutionary app that makes investing in gold hassle-free and rewarding. The app helps Indian households pre-plan their jewellery purchase by saving monthly and gaining 10% extra gold. It provides a user-friendly platform where investors can start saving in gold effortlessly.</p>
+
+    <p>The app operates by offering some of the best gold-saving schemes, allowing for easy accessibility to pure gold both in physical and digital forms without the hassle of visiting physical stores.</p>
+
+    <p>Users can deposit a fixed amount daily, weekly, or monthly, which is then converted into gold and can be redeemed anytime in the form of jewellery, gold coins, or cash at our 300+ trusted jewellery partners.  Additionally, PlusGold offers the advantage of earning an extra 10% gold on every deposit, enhancing the value of the savings.</p>
+
+    <p>This innovative approach allows individuals to accumulate gold systematically, ensuring they are better prepared for future jewellery purchases or financial security.</p>
+
+    <p>By leveraging technology, PlusGold provides a transparent and convenient platform for individuals to invest in gold, catering to the diverse needs of Indian households. With transparent processes and dedicated customer support, we strive to make your investment journey seamless.</p>
+</div>
+
+
+<div>
+<h3>FAQ</h3>
+<p><b>Q: Why invest in gold with Plus Gold? </b><br/>
+Investing in gold with PlusGold offers reliability, transparency, and competitive returns. With our secure platform and lucrative gold schemes, you can build a robust gold investment portfolio with ease.</p>
+<p><b>Q: How do I start a gold savings scheme with Plus Gold?</b><br/>
+
+Starting a gold savings scheme with PlusGold is easy. Simply sign up on our platform, choose a suitable scheme, and initiate your contributions. Our team will guide you through the process every step of the way</p>
+<p><b>Q: Which gold scheme is the best?</b><br/>
+The best gold scheme depends on your investment goals and risk appetite. At PlusGold, we offer a range of schemes tailored to different preferences, ensuring there's an ideal option for every investor</p>
+<p><b>Q: What is the 11-month gold scheme?</b><br/>
+The 10-month gold scheme is a popular investment option offered by PlusGold, allowing investors to deposit funds for the duration of 10 months and earn attractive returns on their investment in gold</p>
+<p><b>Q: How to invest in gold for good returns?</b><br/>
+Investing in gold for good returns involves choosing the right investment vehicle, understanding market trends, and staying updated with economic indicators. PlusGold provides the necessary tools and expertise to maximize your returns from gold investments</p>
+
+</div>
+</div>
+
         </div>
     </div>
 }
