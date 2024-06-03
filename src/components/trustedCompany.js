4d8af0d160c01763781c7937d150f8514653e63d
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-06-03 16:02:11 +0530

    trusted comapny

--- src/components/trustedCompany.js
+++ src/components/trustedCompany.js
@@ -4,7 +4,134 @@ return <div>
 <div className="trusted-companies">
 <h3>Trusted by the best Jewellers</h3>
 <p>Redeem your savings at more than 1000 stores across 520+ cities</p>
+<div className="flex comapny-wrap">
+    <div>
 
+<div className="flex trsuted-company-logos trust-1">
+    <div>
+        <img src={require("../Images/blustsone.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/candere.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/estele.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/grt.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/kalyan.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/relinceJwels.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/tanishq.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/tbz.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/giva.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/joyaluka.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/malbar.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/mia.png")} alt=""/>
+    </div>
+
+</div>
+
+
+<div className="flex trsuted-company-logos">
+    <div>
+        <img src={require("../Images/senco.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/om.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/vipul.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/laaw.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/jv.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/sagar.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/satium.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/shiv.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/shreekardhar.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/shairav.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/vandhana.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/vardan.png")} alt=""/>
+    </div>
+
+</div>
+
+
+
+
+<div className="flex trsuted-company-logos trust-1">
+    <div>
+        <img src={require("../Images/melorra.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/mr.petr.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/muhupat.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/shivngi.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/jia.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/punamaya.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/omalaknkar.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/gunjal.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/kiran.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/mahalaxmi.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/rajawat.png")} alt=""/>
+    </div>
+    <div>
+        <img src={require("../Images/national.png")} alt=""/>
+    </div>
+
+</div>
+</div>
+</div>
 </div>
 
 
